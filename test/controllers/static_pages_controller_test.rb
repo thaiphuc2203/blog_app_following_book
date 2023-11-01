fdbb12a93de1e3418cd77c04b224e4cfe5b81b65
thaiphuc2203 <thaiphuc2203@gmail.com>
2023-11-01 08:55:13 +0700

    re-config testing pages

--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -2,15 +2,15 @@ require "test_helper"
 
 class StaticPagesControllerTest < ActionDispatch::IntegrationTest
   
-  test "should get root" do
-    get static_pages_home_url
-    assert_response :success
-  end
+  # test "should get root" do
+  #   get static_pages_home_url
+  #   assert_response :success
+  # end
   
   test "should get home" do
     get static_pages_home_url
     assert_response :success
-    assert_select "title", "Home | Ruby on Rails Tutorial Sample App"
+    assert_select "title", "Ruby on Rails Tutorial Sample App"
   end
 
   test "should get help" do
